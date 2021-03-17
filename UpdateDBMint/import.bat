78297c2b623355c52dc29f7047f8bc70151019e4
Jordan SAVEA <73700698+J0P0P@users.noreply.github.com>
2021-03-17 10:35:47 +0100

    Update import.bat

--- UpdateDBMint/import.bat
+++ UpdateDBMint/import.bat
@@ -2,19 +2,19 @@
 
 
 REM import data to database for cars
-osm2pgrouting --clean = 1 -f data_nantes_api_converties.osm -d routing_cars -p 5432 -h localhost --username postgres --password jehfp79/_8d3( --conf mapconfig\mapconfig_for_cars.xml --addnodes --attributes --tags
+osm2pgrouting --clean = 1 -f data_nantes_api_converties.osm -d routing_cars -p 5432 -h localhost --username [your username] --password [your password] --conf mapconfig\mapconfig_for_cars.xml --addnodes --attributes --tags
 
 
 REM import data to database for pedestrian
-osm2pgrouting --clean = 1 -f data_nantes_api_converties.osm -d routing_pedestrian -p 5432 -h localhost --username postgres --password jehfp79/_8d3( --conf mapconfig\mapconfig_for_pedestrian.xml --addnodes --attributes --tags
+osm2pgrouting --clean = 1 -f data_nantes_api_converties.osm -d routing_pedestrian -p 5432 -h localhost --username [your username] --password [your password] --conf mapconfig\mapconfig_for_pedestrian.xml --addnodes --attributes --tags
 
 
 REM import data to database for bicycles
-osm2pgrouting --clean = 1 -f data_nantes_api_converties.osm -d routing_bicylces -p 5432 -h localhost --username postgres --password jehfp79/_8d3( --conf mapconfig\mapconfig_for_bicycles.xml --addnodes --attributes --tags
+osm2pgrouting --clean = 1 -f data_nantes_api_converties.osm -d routing_bicylces -p 5432 -h localhost --username [your username] --password [your password] --conf mapconfig\mapconfig_for_bicycles.xml --addnodes --attributes --tags
 
 
 REM import data to database for public transport
-osm2pgrouting --clean = 1 -f data_nantes_api_converties.osm -d routing_public_transport -p 5432 -h localhost --username postgres --password jehfp79/_8d3( --conf mapconfig\mapconfig_for_public_transport.xml --addnodes --attributes --tags
+osm2pgrouting --clean = 1 -f data_nantes_api_converties.osm -d routing_public_transport -p 5432 -h localhost --username [your username] --password [your password] --conf mapconfig\mapconfig_for_public_transport.xml --addnodes --attributes --tags
 
 
 exit
